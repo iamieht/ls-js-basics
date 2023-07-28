b3681f99febc721e1d8b1c38781c28f7c90d6a14
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-28 10:33:18 +0200

    Out of Bounds

---
+++ 1.ReadingDocumentation1/OutOfBounds.js
@@ -0,0 +1,5 @@
+// What happens if we take the array ['fish', 'and', 'chips'] and try to access the element at index position 10?
+
+console.log(["fish", "and", "chips"][3]);
+
+// We won't get any error message, but the expression will return undefined.
