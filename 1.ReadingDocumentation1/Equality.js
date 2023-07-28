d62eb19d77987a57bca41712e52e13a43005d1fe
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-28 11:07:43 +0200

    Equality

---
+++ 1.ReadingDocumentation1/Equality.js
@@ -0,0 +1,3 @@
+//
+console.log("8" == 8); // => true due to loose equality operator. It will implicitely coerce the string '8' to the number 8.
+console.log("8" === 8); // => false (strict equality operator). It compares the type and value of the operands.
