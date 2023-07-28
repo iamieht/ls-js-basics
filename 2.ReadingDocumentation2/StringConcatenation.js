4657861fef6bf55af97deea4076ed3fa7fc862cf
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-28 19:26:40 +0200

    Syntax Error

---
+++ 2.ReadingDocumentation2/StringConcatenation.js
@@ -0,0 +1,15 @@
+// Find out how we can join two or more strings together.
+
+//  The operator + concatenates strings together and the String.prototype.concat() method.
+
+let firstName = "Ada";
+let lastName = "Lovelace";
+
+"".concat(firstName, " ", lastName);
+// 'Ada Lovelace'
+
+let firstName2 = "Ada";
+let lastName2 = "Lovelace";
+
+firstName2 + " " + lastName2;
+// 'Ada Lovelace'
