7c4e2286ae2eb253101ac205094b8d7457e43123
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-28 18:57:53 +0200

    Style Guide

---
+++ 2.ReadingDocumentation2/StyleGuide.js
@@ -0,0 +1,22 @@
+// find all violations of the style guide.
+
+// let ice_cream_taste = 'chocolate'
+// let ice_cream_density = 10
+
+// while(ice_cream_density > 0)
+// {
+//     console.log('Drip...');
+//     ice_cream_density -= 1;
+// }
+
+// console.log('The '+ ice_cream_taste +' ice cream melted.');
+
+const ICE_CREAM_TASTE = "chocolate";
+let iceCreamDensity = 10;
+
+while (iceCreamDensity > 0) {
+  console.log("Drip...");
+  iceCreamDensity -= 1;
+}
+
+console.log("The " + ICE_CREAM_TASTE + " ice cream melted.");
