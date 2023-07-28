c87bc77d6f0acd0d12536f4018be3ea153446339
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-28 19:19:10 +0200

    Argument Signatures

---
+++ 2.ReadingDocumentation2/ArgumentSignatures.js
@@ -0,0 +1,3 @@
+// How many arguments does the Array.prototype.join() method expect? What happens if you call it with less or more arguments?
+
+// It expects one optional argument, which is the separator. If no separator is passed, a "," will be used as default. Any extra argument will be ignored.
