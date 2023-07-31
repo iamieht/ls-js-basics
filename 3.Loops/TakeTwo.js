019adf015406f9b733ff5a0e23dff7e3d1a1412a
Iván E. Hernández T <iamieht@gmail.com>
2023-07-31 11:49:25 +0200

    Create TakeTwo.js
    Loops - Ex4

---
+++ 3.Loops/TakeTwo.js
@@ -0,0 +1,5 @@
+// Write a for loop that iterates over all numbers from 1 to 100, and outputs the result of multiplying each element by 2.
+
+for (let num = 1; num <= 100; num += 1) {
+  console.log(`${num} * 2 = ${num * 2}`);
+}
