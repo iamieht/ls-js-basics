12783e54bf375ba39f801ef163d98100e87dbe6f
Iván E. Hernández T <iamieht@gmail.com>
2023-07-31 12:37:28 +0200

    Create DoWhile.js
    Loops - Ex10

---
+++ 3.Loops/DoWhile.js
@@ -0,0 +1,15 @@
+let counter = 0;
+
+while (counter > 0) {
+  console.log("Woooot!");
+  counter -= 1;
+}
+
+//
+
+let counter2 = 0;
+
+do {
+  console.log("Woooot!");
+  counter2 -= 1;
+} while (counter2 > 0);
