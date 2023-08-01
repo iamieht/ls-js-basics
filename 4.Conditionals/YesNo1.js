1cb15185750fe09c87c4847729f2e528d2bbb18b
Iván E. Hernández T <iamieht@gmail.com>
2023-08-01 10:32:48 +0200

    Create YesNo1.js
    Conditionals - Ex2

---
+++ 4.Conditionals/YesNo1.js
@@ -0,0 +1,8 @@
+// Write an if statement that logs 'Yes!' if randomNumber is 1, and 'No.' if randomNumber is 0.
+let randomNumber = Math.round(Math.random());
+
+if (randomNumber) {
+  console.log("Yes!");
+} else {
+  console.log("No.");
+}
