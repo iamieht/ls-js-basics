aa71affa93c0a439ea6900e38cc31c93f2af27d1
Iván E. Hernández T <iamieht@gmail.com>
2023-08-01 10:41:42 +0200

    Create YesNo2.js
    Conditionals - Ex3

---
+++ 4.Conditionals/YesNo2.js
@@ -0,0 +1,6 @@
+// Write an if statement (ternary operator) that logs 'Yes!' if randomNumber is 1, and 'No.' if randomNumber is 0.
+let randomNumber = Math.round(Math.random());
+
+randomNumber ? console.log("Yes!") : console.log("No.");
+
+console.log(randomNumber ? "Yes!" : "No.");
