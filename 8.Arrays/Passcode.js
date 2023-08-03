8e77cd3a6d1e645df2ad18da547a9b4e7a319934
Iván E. Hernández T <iamieht@gmail.com>
2023-08-03 13:55:42 +0200

    Create Passcode.js
    Arrays - Ex10

---
+++ 8.Arrays/Passcode.js
@@ -0,0 +1,6 @@
+// Write some code that returns a string, with each portion of the passcode separated by a hyphen (-).
+
+let passcode = ["11", "jZ5", "hQ3f*", "8!7g3", "p3Fs"];
+console.log(passcode.join("-"));
+
+// Expected return value: '11-jZ5-hQ3f*-8!7g3-p3Fs'
