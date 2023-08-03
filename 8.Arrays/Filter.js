14eef306a5d342601169c62770060a374a870933
Iván E. Hernández T <iamieht@gmail.com>
2023-08-03 11:22:14 +0200

    Create Filter.js
    Arrays - Ex5

---
+++ 8.Arrays/Filter.js
@@ -0,0 +1,5 @@
+// Count the number of elements in scores that are 100 or above.
+let scores = [96, 47, 113, 89, 100, 102];
+let scoresOver100 = scores.filter((num) => num >= 100).length;
+
+console.log(scoresOver100);
