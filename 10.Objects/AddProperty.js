713ef3aef96a7005f784b7efc5e1866062e3d84d
Iván E. Hernández T <iamieht@gmail.com>
2023-08-04 11:00:51 +0200

    Create AddProperty.js
    Objects - Ex3

---
+++ 10.Objects/AddProperty.js
@@ -0,0 +1,14 @@
+let fido = {
+  name: "Fido",
+  species: "Labrador Retriever",
+  color: "brown",
+  weight: 16,
+};
+
+// Add property 'age'.
+fido["age"] = 2;
+
+// Add property 'favorite food'.
+fido["favorite food"] = "meat";
+
+console.log(fido);
